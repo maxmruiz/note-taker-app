1b6f6910d841d40f5bff763e65488e148fa6ea8a
max martinez <mmfern2003@gmail.com>
2023-12-20 16:06:22 -0500

    adding routes for deleting premade notes as well as tidying up the code

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Path to db.json file
 const dbPath = path.join(__dirname, 'db', 'db.json');
 
-// Function to read notes from db.json
-function readNotes() {
+// Helper function to read notes from the db.json file
+const readNotes = () => {
     return new Promise((resolve, reject) => {
-        fs.readFile(dbPath, 'utf8', (err, data) => {
+        fs.readFile(dbPath, (err, data) => {
             if (err) {
                 reject(err);
             } else {
@@ -24,12 +22,12 @@ function readNotes() {
             }
         });
     });
-}
+};
 
-// Function to write notes to db.json
-function writeNotes(notes) {
+// Helper function to write notes to the db.json file
+const writeNotes = (notes) => {
     return new Promise((resolve, reject) => {
-        fs.writeFile(dbPath, JSON.stringify(notes, null, 2), 'utf8', (err) => {
+        fs.writeFile(dbPath, JSON.stringify(notes, null, 2), (err) => {
             if (err) {
                 reject(err);
             } else {
@@ -37,32 +35,52 @@ function writeNotes(notes) {
             }
         });
     });
-}
+};
 
-// GET route for fetching notes
+// GET endpoint to fetch all notes
 app.get('/api/notes', async (req, res) => {
     try {
         const notes = await readNotes();
         res.json(notes);
     } catch (err) {
-        res.status(500).json({ message: "Failed to read notes" });
+        res.status(500).json({ error: "Failed to read notes" });
     }
 });
 
-// POST route to save a note
+// POST endpoint to create a new note or update an existing note
 app.post('/api/notes', async (req, res) => {
+    const { title, text, id } = req.body;
     try {
-        const newNote = req.body;
-        const notes = await readNotes();
-        notes.push(newNote); // Add the new note to the array
+        let notes = await readNotes();
+        if (id) {
+            // Update existing note
+            notes = notes.map(note => note.id === id ? { title, text, id } : note);
+        } else {
+            // Create new note
+            const newNote = { title, text, id: Date.now().toString() }; // Simple ID generation
+            notes.push(newNote);
+        }
         await writeNotes(notes);
-        res.json({ message: 'Note saved successfully' });
+        res.json({ message: "Note saved successfully" });
     } catch (err) {
-        res.status(500).json({ message: "Failed to save note" });
+        res.status(500).json({ error: "Failed to save the note" });
+    }
+});
+
+// DELETE endpoint to delete a note
+app.delete('/api/notes/:id', async (req, res) => {
+    const noteId = req.params.id;
+    try {
+        let notes = await readNotes();
+        notes = notes.filter(note => note.id !== noteId);
+        await writeNotes(notes);
+        res.json({ message: "Note deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ error: "Failed to delete the note" });
     }
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
